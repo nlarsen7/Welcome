0541be13fc3024ab15ad44d8f4d24f2dd0e48de6
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 19:56:53 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func welcome() {
+  fmt.Println(" Welcome to this program! This is a nice, warm welcome for you and introduces you to my program. This has to do with making functions and recalling them later in another function. Thank you for being here and i hope you enjoy the program!")
+}
+func main() {
+  welcome()
+}
