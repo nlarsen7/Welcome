94e3b1f19aa012c8b5fb1c1b0d888a1b227d3c2e
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 20:00:21 +0000

    Welcome 3.2.2

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
+//Nicholas Larsen
+//4/7/2020
+//a welcome message
 package main
 
 import "fmt"
 
 func welcome() {
   fmt.Println(" Welcome to this program! This is a nice, warm welcome for you and introduces you to my program. This has to do with making functions and recalling them later in another function. Thank you for being here and i hope you enjoy the program!")
+  //welcome message for the user
 }
 func main() {
   welcome()
+  //does what the welcome function does
 }
